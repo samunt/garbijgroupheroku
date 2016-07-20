1602d4d60183458fa5458df8def43430c31ceb44
Enakshi Shah <enakshi.shah@mail.utoronto.ca>
2016-07-20 14:01:37 -0400

    added error messages to sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ before_action :require_login, only: [:edit, :update]
     if @user.save
       UserMailer.welcome_email(@user).deliver_later
       self.current_user = @user
-      redirect_to root_path, alert: 'Signed up'
+      redirect_to root_path, alert: 'Signed up!'
     else
+      flash[:alert] = "Sign up Failed!"
       render :new
     end
   end
