43ac413f2e3848c0aa8ee979d8f5919348697680
Impuls3iv <sammy.unterman@gmail.com>
2016-07-21 12:52:06 -0400

    auto log-in after user signup

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,6 +1,9 @@
 class Space < ActiveRecord::Base
   has_many :buy_transactions, class_name: 'Transaction', foreign_key: 'buy_space_id'
   has_many :sell_transactions, class_name: 'Transaction', foreign_key: 'sell_space_id'
+
+  geocoded_by :address
+
   belongs_to :user
 
 end
