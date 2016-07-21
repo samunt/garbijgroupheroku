ebbdc1f89fa986032fda1941c309880c27418fe9
Seila <seila.tupone@gmail.com>
2016-07-21 10:03:35 -0400

    Change garbaje_day data type in string

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721000559) do
+ActiveRecord::Schema.define(version: 20160721134322) do
 
   create_table "spaces", force: :cascade do |t|
     t.integer  "capacity"
-    t.date     "garbaje_day"
+    t.string   "garbaje_day"
     t.integer  "user_id"
     t.string   "address"
     t.datetime "created_at",  null: false
