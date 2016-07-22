45d4d9584288475f234562fa5814050d0b2a9c89
Impuls3iv <sammy.unterman@gmail.com>
2016-07-22 15:55:28 -0400

    maps....

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -11,9 +11,24 @@ function initMap(){
     });
 
     // var arrayLat =
-
+    var markers = [];
+    var infoWindowContent = [];
+    $.each(spaces, function(index)){
+      markers.push([spaces[index]['address'], spaces[index]['latitude'], spaces[index]['longitude']]);
+      // infoWindowContent.push([])
+    }
 
   var Xname = mapPlaceHolder.data('name')
+
+
+  //
+  // var markers = new google.maps.Marker({[
+  //         ['<%= @spaces[0].title %>', '<%= @spaces[0].latitude %>','<%= @spaces[0].longitude %>'],
+  //         ['<%= @spaces[1].title %>', '<%= @spaces[1].latitude %>','<%= @spaces[1].longitude %>'],
+  //         ['<%= @spaces[2].title %>', '<%= @spaces[2].latitude %>','<%= @spaces[2].longitude %>'],
+  //         ['<%= @spaces[3].title %>', '<%= @spaces[3].latitude %>','<%= @spaces[3].longitude %>'],
+  //         ['<%= @spaces[4].title %>', '<%= @spaces[4].latitude %>','<%= @spaces[4].longitude %>']
+  //       ]});
 
 
   var marker = new google.maps.Marker({
@@ -23,11 +38,3 @@ function initMap(){
   });
 
 };
-//
-// var markers = [
-//         ['<%= @spaces[0].title %>', '<%= @spaces[0].latitude %>','<%= @spaces[0].longitude %>'],
-//         ['<%= @spaces[1].title %>', '<%= @spaces[1].latitude %>','<%= @spaces[1].longitude %>'],
-//         ['<%= @spaces[2].title %>', '<%= @spaces[2].latitude %>','<%= @spaces[2].longitude %>'],
-//         ['<%= @spaces[3].title %>', '<%= @spaces[3].latitude %>','<%= @spaces[3].longitude %>'],
-//         ['<%= @spaces[4].title %>', '<%= @spaces[4].latitude %>','<%= @spaces[4].longitude %>']
-//       ];
