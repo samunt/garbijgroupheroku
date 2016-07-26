41beee4b52a0c4b0d5ff71d5a703dc2553b2ee51
Benny <bennyxnguyen@gmail.com>
2016-07-25 21:31:07 -0400

    adding scroll javascript

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,27 @@
+$(document).on('ready', function() {
+var $animation_elements = $('.when-you-sell');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('fadeInLeft');
+    }
+  });
+}
+
+$window.on('scroll', check_if_in_view);
+$window.trigger('scroll');
+
+});
