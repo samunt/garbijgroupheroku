25e42115d651ea647c7dd07c436049e911d11840
Seila <seila.tupone@gmail.com>
2016-07-28 13:49:05 -0400

    Uncomment welcome controller transaction animation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @user = User.new
-    # @homepage = true
+    @homepage = true
     # @transaction_money = Transaction.all
     # @sum = 0 #counter for the how much we saved you line on the welcome page
     # @transaction_money.each do |x|
