e146d4e50b509ca66913161a5bd0eec8be7d8b43
Impuls3iv <sammy.unterman@gmail.com>
2016-07-28 21:33:30 -0400

    added validations

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,12 @@
 class Space < ActiveRecord::Base
+
+  validates :capacity, numericality: { only_integer: true }
+  validates :address, presence: true
+  validates :user_id, numericality: { only_integer: true }
+  validates :latitude, numericality: { only_integer: true }
+  
+
+
   has_many :buy_transactions, class_name: 'Transaction', foreign_key: 'buy_space_id'
   has_many :sell_transactions, class_name: 'Transaction', foreign_key: 'sell_space_id'
 
