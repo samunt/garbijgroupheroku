cddadb0cf183dea996e70adb894ed7f739cf8e29
Impuls3iv <sammy.unterman@gmail.com>
2016-08-19 19:51:19 -0400

    problem with garbage marker in production. so changed path and added /images

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -31,7 +31,7 @@ function makeMap(){
       position: position,
       // returns address on hover
       title: markers[i][0],
-      icon: '/assets/garbage-marker-22px.png',
+      icon: '/assets/images/garbage-marker-22px.png',
       map: map
       });
 
